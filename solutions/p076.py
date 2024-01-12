fc87eb47ec59ee93915e2ae78e8b19c38abc6458
rui <ruiwang493@gmail.com>
2024-01-12 05:22:50 -0600

    p076 explanation fix

--- solutions/p076.py
+++ solutions/p076.py
@@ -1,6 +1,6 @@
 """
 This problem can be solved with the mathematical concept of the partition function p(n), where you partition a number n into piles that sum up to n. We can implement the partition function and simply count the value of p(100) (subtracted by 1 since p(n) includes the configuration of one pile: n).
-The partition function p(n), proven by Euler's pentagonal number theorem, can be expressed in terms of the partition functions of n - g_k where g_k is the kth generalized pentagonal number.
+The partition function p(n), proven by Euler's pentagonal number theorem, can be expressed in terms of the partition functions of n - g_k where g_k is the kth generalized pentagonal number. We can cache these values when we compute them to speed up computation.
 Pentagonal numbers are calculated by p_n = n(3n-1)/2 and are generalized by taking negative numbers as well, with the sequence of generalized pentagonal numbers being this function on the sequence 0, 1, -1, 2, -2, etc. We can cache this in the implementation.
 """
 import functools
