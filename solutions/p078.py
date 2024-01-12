0703eb71dd1c7174abf5198fd4aa9e1db938b09b
rui <ruiwang493@gmail.com>
2024-01-12 05:22:12 -0600

    p078

---
+++ solutions/p078.py
@@ -0,0 +1,36 @@
+"""
+This problem can be solved with the mathematical concept of the partition function p(n), where you partition a number n into piles that sum up to n. We can implement the partition function and simply count off numbers until we reach n such that p(n) % 10 ** 6 = 0
+The partition function p(n), proven by Euler's pentagonal number theorem, can be expressed in terms of the partition functions of n - g_k where g_k is the kth generalized pentagonal number, and we can cache these values to speed up the computation.
+Pentagonal numbers are calculated by p_n = n(3n-1)/2 and are generalized by taking negative numbers as well, with the sequence of generalized pentagonal numbers being this function on the sequence 0, 1, -1, 2, -2, etc. We can cache this in the implementation.
+"""
+import functools
+import itertools
+
+@functools.cache
+def pentagonal(i):
+    """Returns the generalized pentagonal number given by the value i"""
+    return i*(3*i - 1)//2
+
+@functools.cache
+def partition(n):
+    """Recursively computes the partition function of n"""
+    if n < 0:
+        return 0
+    if n == 0 or n == 1:
+        return 1
+    s = 0
+    i = 1
+    neg = 1
+    while n - pentagonal(i) >= 0 or n - pentagonal(-i) >= 0:
+        s += neg*(partition(n - pentagonal(i)) + partition(n - pentagonal(-i)))
+        i += 1
+        neg *= -1
+    return s
+
+def solve():
+    for i in itertools.count(1):
+        if partition(i) % 10**6 == 0:
+            return i
+
+if __name__ == "__main__":
+    print(solve())
